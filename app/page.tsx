14b7a1e1f5ebaa4e9e5111e590afa876fa9afeb2
davidhickey <davidhickey423@gmail.com>
2025-04-09 18:38:38 -0400

    removes console.log

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,11 @@
 //api flow > ui initiates > useHook > api methods -> server api http req
 //upload a csv that adds DB data
 
-import { getUsers } from "@/lib/prisma/methods";
 import LeadsMain from "@/components/features/Leads/LeadsMain";
 import leads from "@/lib/prisma/sampleData";
 
 
 export default async function Home() {
-  const users = await getUsers();
-  console.log(users);
-
   return (
     <div>
       <main>
